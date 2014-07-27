962534ad7c847511b132259311ebd14d4918f96f
HaiMing.Yin <HaiMing.Yin@gmail.com>
2014-07-28 03:00:39 +0800

    testing store

--- stratum/store.go
+++ stratum/store.go
@@ -3,3 +3,29 @@
 // go get github.com/tecbot/gorocksdb
 package stratum
 
+import (
+	"github.com/golang/glog"
+	"os"
+	rocksdb "github.com/tecbot/gorocksdb"
+)
+
+type Store struct {
+	*rocksdb.DB
+}
+
+func NewStore() *Store {
+	dbName := os.TempDir() + "/ninepool"
+	options := rocksdb.NewDefaultOptions()
+	options.SetCreateIfMissing(true)
+	db, err := rocksdb.OpenDb(options, dbName)
+	if err != nil {
+		glog.Fatalf("Can not open db: %s", err)
+	}
+	return &Store{db}
+}
+
+func (db *Store) put(key, value []byte) error {
+	o := rocksdb.NewDefaultWriteOptions()
+	return db.Put(o, key, value)
+}
+
